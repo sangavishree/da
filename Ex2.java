f3a94d63dc6db1d5c10fde0ff54ef8918b86a352
sangavishree <40466356+sangavishree@users.noreply.github.com>
2018-06-21 19:50:08 +0530

    Create Ex2.java

---
+++ Ex2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Ex2
+{
+public static void main(String args[])
+{
+int n;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+System.out.println(n);
+if(n%2==0)
+{
+System.out.println("even number");
+}
+else
+System.out.println("odd");
+}
+}
