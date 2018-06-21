c789b31e15447d46a39d95190d28a3b5fa140c5e
sangavishree <40466356+sangavishree@users.noreply.github.com>
2018-06-21 19:55:45 +0530

    Create Ex3.java

---
+++ Ex3.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Ex3
+{
+publlic static void main(String args[])
+{
+if(n==a||e'||'i'||'o'||'u'||'A'||'E'||'I'||'O'||'U')
+{
+System.out.println("it is a vowel");
+}
+else
+System.out.println("not a vowel");
+}
+}
