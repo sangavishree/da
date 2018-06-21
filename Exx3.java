afd50ab94eeb0f4f79b7c6251065b408359b8b4c
sangavishree <40466356+sangavishree@users.noreply.github.com>
2018-06-21 20:50:01 +0530

    Rename Ex3.java to Exx3.java

---
+++ Exx3.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Ex3
+{
+public static void main(String args[])
+{
+char n='i';
+if(n=='a'||n=='e'||n=='i'||n=='o'||n=='u'||n=='A'||n=='E'||n=='I'||n=='O'||n=='U')
+{
+System.out.println("it is a vowel");
+}
+else
+System.out.println("not a vowel");
+}
+}
