3b6145ae849916b35828aa1a89d0cadd15a74a34
sangavishree <40466356+sangavishree@users.noreply.github.com>
2018-06-21 21:23:48 +0530

    Create Ex8.java

---
+++ Ex8.java
@@ -0,0 +1,12 @@
+class Ex8
+{
+public static void main(String args[])
+{
+int num,n;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+System.out.println(n);
+num=n*(n+1)/2;
+System.out.println(num);
+}
+}
