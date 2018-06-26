cabec4055e80d5067fd5242a566e78a031755baf
sangavishree <40466356+sangavishree@users.noreply.github.com>
2018-06-26 19:42:45 +0530

    Create h4.java

---
+++ h4.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class h4 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		int a[]=new int[N];
+		for(int i=0;i<N;i++){
+			a[i]=sc.nextInt();
+		}
+		for(int i=0;i<N;i++){
+			int e=0;
+			for(int j=i+1;j<N;j++){
+				if(a[i]==a[j]){
+					a[j]=0;
+					
+					e++;
+					
+				}
+			}
+			if(e==0){
+				if(a[i]!=0)
+				System.out.print(a[i]);
+			}
+		}
+
+}
